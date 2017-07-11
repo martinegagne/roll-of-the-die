bea1c01c0b7f67793d50003b8d9724ed0b5ec98f
Martine Gagne <martine.e.gagne@gmail.com>
2017-07-10 23:25:00 -0400

    create second dice feature

---
+++ dice.rb
@@ -0,0 +1,8 @@
+
+die_num_one = Random.rand(6) + 1
+
+die_num_two = Random.rand(6) + 1
+
+
+puts "You have rolled a #{die_num_one} and a #{die_num_two}!"
+puts "Your total is (#{die_num_one + die_num_two})"
